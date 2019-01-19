eac2b3d5f084fbaf4d24f9091e9b5b8d80a6954a
Arsalan Fakhar <arsalan.fakhar@yahoo.com>
2019-01-19 12:59:16 +0500

    Reduced code size and added validations where necessary

--- js/main.js
+++ js/main.js
@@ -163,16 +163,17 @@ var user;
 
 //Adding a authentication listener
 auth.onAuthStateChanged(firebaseUser=>{
+    
     if(firebaseUser){
         user=firebaseUser;
      
         console.log(firebaseUser.email);
         //Check for main_page
         //when hosting set it to location.pathname
-        if(location.href=="file:///F:/Blood%20bank%20site/main_page.html")
-        {
-            setDisplayName();
-        }
+         if(location.href=="file:///F:/Blood%20bank%20site/main_page.html")
+         {
+             setDisplayName();
+         }
 
 
     }
@@ -233,24 +234,116 @@ function logout_user(){
 
 
 function setDisplayName(){
-    if(user==null){
-        setInterval(setDisplayName(),1000);
-    }
-    else{
+    
+    
+    
+    
         if(user.displayName=="" ||user.displayName==null ){
+            
             document.getElementById('display_username').innerHTML="Username";
             }
             else{
             document.getElementById('display_username').innerHTML=user.displayName;
             }
-    }
+    
+    
+        
+    
     
 } 
+
+/*Changes start */
+const  database = firebase.database();
+
+var data;
+database.ref().on('value',function(snapshot){
+    data=snapshot.val();
+    
+
+})
+
+function regDonor(){
+
+    database.ref('Records/'+user.uid).once('value',function(snapshot){
+        
+        if(snapshot.exists()){
+            var mydata=snapshot.val();
+            //Precaution but not necessary
+            if(mydata.email==user.email){
+                swal({
+                    title: "Already Registered",
+                    text: "Kitni bar khun dega mar jaye ga",
+                    icon: "error"
+                  })
+            }
+            
+            //console.log('Mera');
+            //console.log(snapshot.val());   
+        }
+        else{
+            location.assign("donor_reg_form.html");
+        }
+
+    })
+   // location.assign("trial_donor.html");
+}
+/*Changes end */
+
+
 
 //Main_page_js_end
 
 //Donor_page_js_start
-function showage(){ 
+
+var firstName;var lastName;var dateob;var email_donors;var address_donor;
+var address_donor;var age_donor;var phone_donor;var blood_grp;
+
+function getValues(){
+    
+    firstName=document.getElementById("firstname").value;
+     lastName=document.getElementById("lastname").value;
+     dateob=document.getElementById("dateofb").value;
+     email_donors=document.getElementById('email_donor').value;
+
+     address_donor=document.getElementById("address").value;
+     age_donor=document.getElementById("age").value;
+     phone_donor=document.getElementById("phone_number").value;
+     blood_grp=document.getElementById("selectpicker").value;
+}
+
+function submitData(){
+    getValues();
+    database.ref('Records/'+user.uid).set({
+        name:firstName+" "+lastName,
+        DateOfbirth:dateob,
+        email:email_donors,
+        Address:address_donor,
+        Age:age_donor,
+        Phone_Number:phone_donor,
+        Blood_Group:blood_grp
+
+    })
+    .then(()=>{
+        swal({
+            title: "Congratulations",
+            text: "You Saved a life!",
+            icon: "success",
+          })
+          .then(res=>{
+            location.replace("main_page.html");
+          })
+    })
+}
+
+
+
+
+
+
+
+
+
+/* function showage(){ 
   
  
 var today=new Date();
@@ -267,99 +360,102 @@ var Age=c_year-arr[0];
 document.getElementById("age").value=Age;
 
 
-}
-function regDonor(){
-    location.assign("donor_reg_form.html");
-}
+} */
+// function regDonor(){
+//     location.assign("donor_reg_form.html");
+// }
 var firebaseRef=firebase.database().ref();
 //retrieve();
-var counts;
-firebaseRef.child("Records").on('value', function(snapshot) { counts= snapshot.numChildren(); });
-//var num=firebaseRef.child("Records").datasnapshot.getChildrenCount();
-var checking=0;
-function submit (){
+// var counts;
+// firebaseRef.child("Records").on('value', function(snapshot) { counts= snapshot.numChildren(); });
+// //var num=firebaseRef.child("Records").datasnapshot.getChildrenCount();
+// var checking=0;
 
-var  email=document.getElementById("email_donor").value;
+// function submit (){
 
-
-
+// var  email=document.getElementById("email_donor").value;
 
 
-var isregistered=()=>
-{
-    this.checking=0;
-    //var t=1;
+
+
+
+// var isregistered=()=>
+// {
+//     this.checking=0;
+//     //var t=1;
     
-    firebaseRef.child("Records").once('value', function(snapshot){
-if(snapshot.exists()){
-    var content = '';
+//     firebaseRef.child("Records").once('value', function(snapshot){
+// if(snapshot.exists()){
+//     var content = '';
 
-    snapshot.forEach(function(data){
-        var val = data.val();
-       // alert(email);
-       // alert(val.Email);
-     if(val.Email===email){
-       //  alert("in email matching if");
+//     snapshot.forEach(function(data){
+//         var val = data.val();
+//        // alert(email);
+//        // alert(val.Email);
+//      if(val.Email===email){
+//        //  alert("in email matching if");
        
-    this.checking=1;
-    }
-    });
+//     this.checking=1;
+//     }
+//     });
    
 
-}
+// }
 
-});
-
-
-
-
-}
-
-isregistered();
+// });
 
 
 
 
-if(checking){
-alert("Already Registered");
-}
-else{
-var firstName=document.getElementById("firstname").value;
-var lastName=document.getElementById("lastname").value;
-var dateOfBirth=document.getElementById("dateofb").value;
+// }
 
-var address=document.getElementById("address").value;
-var age=document.getElementById("age").value;
-var phone_number=document.getElementById("phone_number").value;
-var bloodGroup=document.getElementById("selectpicker").value;
-if(firstName==""||lastName==""||dateOfBirth==""||address==""||age==""||phone_number==""||bloodGroup=="")
-{
-alert("some fields are empty!");
-}
-else{
-firebaseRef.child("Records//"+(++counts)).set({name:firstName+lastName,DateOfbirth:dateOfBirth,Email:email,Address:address,Age:age,Phone_Number:phone_number,Blood_Group:bloodGroup});
-//firebaseRef.child("Records").set({name:firstName+lastName,DateOfbirth:"dateOfBirth",Email:"email"});
-alert("Registered");
-location.replace("main_page.html");
-
-/*swal({
-    title: "Congratulations",
-    text: "You are Registered.!",
-    icon: "success",
+// isregistered();
 
 
-  })
-  .then((data) => {
-    if (data) {
+
+
+// if(checking){
+// alert("Already Registered");
+// }
+// else{
+// var firstName=document.getElementById("firstname").value;
+// var lastName=document.getElementById("lastname").value;
+// var dateOfBirth=document.getElementById("dateofb").value;
+
+// var address=document.getElementById("address").value;
+// var age=document.getElementById("age").value;
+// var phone_number=document.getElementById("phone_number").value;
+// var bloodGroup=document.getElementById("selectpicker").value;
+// if(firstName==""||lastName==""||dateOfBirth==""||address==""||age==""||phone_number==""||bloodGroup=="")
+// {
+// alert("some fields are empty!");
+// }
+// else{
+// firebaseRef.child("Records//"+(++counts)).set({name:firstName+lastName,DateOfbirth:dateOfBirth,Email:email,Address:address,Age:age,Phone_Number:phone_number,Blood_Group:bloodGroup});
+// //firebaseRef.child("Records").set({name:firstName+lastName,DateOfbirth:"dateOfBirth",Email:"email"});
+// alert("Registered");
+// location.replace("main_page.html");
+
+// /*swal({
+//     title: "Congratulations",
+//     text: "You are Registered.!",
+//     icon: "success",
+
+
+//   })
+//   .then((data) => {
+//     if (data) {
     
-      location.replace("main_page.html");
-  }
-}); */
+//       location.replace("main_page.html");
+//   }
+// }); */
 
 
-}
-}
-}/*
+// }
+// }
+// }//submit end
+
+/*
 ()=>{
     if(document.getElementById("blood_grp").value=="Default"){var n="default";retrieve("Default");}
 }
@@ -376,16 +472,18 @@ function retrieve()
     var t=1;
   
     firebaseRef.child("Records").once('value', function(snapshot){
-if(snapshot.exists()){
-    var s='<tr style="border:1px solid black" id="tr" >'+
-          '<th style="border:1px solid black ">#</th>'+
-          '<th style="border:1px solid black ">Names</th>'+
-          '<th style="border:1px solid black">Blood Group</th>' +
-          '<th style="border:1px solid black">Age</th>' +
-          '<th style="border:1px solid black">Phone Number</th>'+
-          '<th style="border:1px solid black">Address</th>' +
-          '<th style="border:1px solid black">Email</th>'
-    var content = '';
+        var s='<tr style="border:1px solid black" id="tr" >'+
+        '<th style="border:1px solid black ">#</th>'+
+        '<th style="border:1px solid black ">Names</th>'+
+        '<th style="border:1px solid black">Blood Group</th>' +
+        '<th style="border:1px solid black">Age</th>' +
+        '<th style="border:1px solid black">Phone Number</th>'+
+        '<th style="border:1px solid black">Address</th>' +
+        '<th style="border:1px solid black">Email</th>'
+         var content = '';
+
+    if(snapshot.exists()){
+   
     
 
     snapshot.forEach(function(data){
@@ -402,7 +500,7 @@ if(snapshot.exists()){
         content += '<td style="border:1px solid black">' + val.Age+ '</td>';
         content += '<td style="border:1px solid black">' + val.Phone_Number+ '</td>';
         content += '<td style="border:1px solid black">' + val.Address+ '</td>';
-        content += '<td style="border:1px solid black">' + val.Email+ '</td>';
+        content += '<td style="border:1px solid black">' + val.email+ '</td>';
 
      
         
@@ -426,9 +524,6 @@ if(snapshot.exists()){
                 t++;
 
       }
-
-
-        
      
     });
     //$('#ex-table').append(content);
@@ -437,11 +532,16 @@ if(snapshot.exists()){
    if(content!="")
     document.getElementById("ex-table").innerHTML=s+content;
  else{
-    document.getElementById("ex-table").innerHTML=s+"<h1>No Record<h1>";
+    //document.getElementById("ex-table").innerHTML=s+"<h1>No Record<h1>";
+    document.getElementById("ex-table").innerHTML=s+'<tr>'+'<td colspan="7">'+'<h1 class="text-center">No Record<h1>'+ '</td>'+ '</tr>';
  }
     
      
 
+}
+else{
+    s+='</tr>';
+    document.getElementById("ex-table").innerHTML=s+'<tr>'+'<td colspan="7">'+'<h1 class="text-center">No Record<h1>'+ '</td>'+ '</tr>';
 }
 
 
